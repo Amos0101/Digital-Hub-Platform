0a0d845b64a3382a209cfceb6ba5d7039746fd3b
Amos0101 <amoskilonzo370@gmail.com>
2026-06-18 12:13:59 +0300

    Configured database for hosting4

--- digital_hub/settings.py
+++ digital_hub/settings.py
@@ -56,11 +56,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'digital_hub.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
